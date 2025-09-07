68df3ceb3855958997e2b2ee82356e998f3aa036
thanphuuuuu <tranlethanhphu.252005@gmil.com>
2025-09-07 16:33:48 +0700

    first commit

---
+++ backend/src/server.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import taskRoute from './routes/tasksRouters.js'
+import { connectDB } from './config/db.js'
+import dotenv from 'dotenv'
+import cors from 'cors'
+
+dotenv.config()
+
+const PORT = process.env.PORT || 5001
+
+const app = express()
+
+
+// middlewares
+app.use(express.json())
+app.use(cors({ origin: "http://localhost:5173" }))
+
+app.use("/api/tasks", taskRoute)
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server bắt đầu trên cổng ${PORT}`)
+  })
+})
---
+++ fontend/src/lib/data.js
@@ -0,0 +1,26 @@
+export const FilterType = {
+  all: "tất cả",
+  active: "đang làm",
+  completed: "hoàn thành",
+}
+
+export const options = [
+  {
+    value: "today",
+    label: "hôm nay",
+  },
+  {
+    value: "week",
+    label: "tuần này",
+  },
+  {
+    value: "month",
+    label: "tháng này",
+  },
+  {
+    value: "all",
+    label: "tất cả",
+  },
+]
+
+export const visibleTaskLimit = 4
